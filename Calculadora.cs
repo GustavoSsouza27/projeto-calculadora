dafdd9acd8543ac82aee15a2c26074dc549f13cb
Gustavo Santos <santosgustavo52579@gmail.com>
2025-08-06 21:44:27 -0300

    Foi realizado validação de dados, bloqueio de letras e caracteres.

--- Calculadora.cs
+++ Calculadora.cs
@@ -9,6 +9,12 @@ namespace Calculadora
 
         private void btnSomar_Click(object sender, EventArgs e)
         {
+            if (string.IsNullOrWhiteSpace(txtNumero1.Text) || string.IsNullOrWhiteSpace(txtNumero2.Text))
+            {
+                MessageBox.Show("O campo está vazio ou só tem espaços!");
+            }
+            
+
             //Verifica se os campos de entrada estão preenchidas
             double numero1 = Convert.ToDouble(txtNumero1.Text);
             double numero2 = Convert.ToDouble(txtNumero2.Text);
@@ -20,8 +26,19 @@ namespace Calculadora
 
         private void btnSubtrair_Click(object sender, EventArgs e)
         {
-            double numero1 = Convert.ToDouble(txtNumero1.Text);
-            double numero2 = Convert.ToDouble(txtNumero2.Text);
+            if (string.IsNullOrWhiteSpace(txtNumero1.Text) || string.IsNullOrWhiteSpace(txtNumero2.Text))
+            {
+                MessageBox.Show("O campo está vazio ou só tem espaços!");
+            }
+
+            if (!double.TryParse(txtNumero1.Text, out double numero1) || !double.TryParse(txtNumero2.Text, out double numero2))
+            {
+                MessageBox.Show("Digite apenas números válidos! Letras não são permitidas.");
+                return;
+            }
+
+            _ = Convert.ToDouble(txtNumero1.Text);
+            _ = Convert.ToDouble(txtNumero2.Text);
 
 
             lblResultado.Text = (numero1 - numero2).ToString();
@@ -29,8 +46,19 @@ namespace Calculadora
 
         private void btnMultiplicar_Click(object sender, EventArgs e)
         {
-            double numero1 = Convert.ToDouble(txtNumero1.Text);
-            double numero2 = Convert.ToDouble(txtNumero2.Text);
+            if (string.IsNullOrWhiteSpace(txtNumero1.Text) || string.IsNullOrWhiteSpace(txtNumero2.Text))
+            {
+                MessageBox.Show("O campo está vazio ou só tem espaços!");
+            }
+
+            if (!double.TryParse(txtNumero1.Text, out double numero1) || !double.TryParse(txtNumero2.Text, out double numero2))
+            {
+                MessageBox.Show("Digite apenas números válidos! Letras não são permitidas.");
+                return;
+            }
+
+            _ = Convert.ToDouble(txtNumero1.Text);
+            _ = Convert.ToDouble(txtNumero2.Text);
 
 
             lblResultado.Text = (numero1 * numero2).ToString();
@@ -40,8 +68,19 @@ namespace Calculadora
 
         private void btnDividir_Click(object sender, EventArgs e)
         {
-            double numero1 = Convert.ToDouble(txtNumero1.Text);
-            double numero2 = Convert.ToDouble(txtNumero2.Text);
+            if (string.IsNullOrWhiteSpace(txtNumero1.Text) || string.IsNullOrWhiteSpace(txtNumero2.Text))
+            {
+                MessageBox.Show("O campo está vazio ou só tem espaços!");
+            }
+
+            if (!double.TryParse(txtNumero1.Text, out double numero1) || !double.TryParse(txtNumero2.Text, out double numero2))
+            {
+                MessageBox.Show("Digite apenas números válidos! Letras não são permitidas.");
+                return;
+            }
+
+            _ = Convert.ToDouble(txtNumero1.Text);
+            _ = Convert.ToDouble(txtNumero2.Text);
             lblResultado.Text = (numero1 / numero2).ToString();
         }
     }
